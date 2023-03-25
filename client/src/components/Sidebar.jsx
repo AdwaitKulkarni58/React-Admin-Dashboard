dcbe8365f564bf3eef1462dc7d7fa6fae189c396
Adwait <adwait.kul.2018@gmail.com>
2023-03-25 15:30:43 -0700

    add component code

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -32,8 +32,37 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const Sidebar = (drawerWidth, isSidebarOpen, setIsSidebarOpen, isNonMobile) => {
+  const { pathName } = useLocation();
+  const [active, setActive] = useState("");
+  const navigate = useNavigate();
+  const theme = useTheme();
+
+  useEffect(() => {
+    setActive(pathName.substring(1));
+  }, [pathName]);
+  return (
+    <Box component="nav">
+      {isSidebarOpen && (
+        <Drawer
+          open={isSidebarOpen}
+          onClose={() => setIsSidebarOpen(false)}
+          variant="persistent"
+          anchor="left"
+          sx={{
+            width: drawerWidth,
+            "@ .MuiDrawer-paper": {
+              color: theme.palette.secondary[200],
+              backgroundColor: theme.palette.background.alt,
+              boxSizing: "border-box",
+              borderWidth: isNonMobile ? 0 : "2px",
+              width: drawerWidth,
+            },
+          }}
+        ></Drawer>
+      )}
+    </Box>
+  );
 };
 
 export default Sidebar;
