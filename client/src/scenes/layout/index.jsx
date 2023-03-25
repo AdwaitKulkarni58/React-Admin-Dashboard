159bd46350388531be0a30675daf132fc5d44236
Adwait <adwait.kul.2018@gmail.com>
2023-03-25 14:00:58 -0700

    start work on layout

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import Navbar from "components/Navbar";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <Box width="100%" height="100%">
+      <Box>
+        <Navbar />
+      </Box>
+    </Box>
+  );
 };
 
 export default Layout;
