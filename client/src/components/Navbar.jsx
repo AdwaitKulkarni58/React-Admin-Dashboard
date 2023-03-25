9a8a2dcbd114c5071546241997159c6b1a1297bf
Adwait <adwait.kul.2018@gmail.com>
2023-03-25 14:11:58 -0700

    work on sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,17 +6,29 @@ import {
   Search,
   SettingsOutlined,
   ArrowDropDownOutlined,
-  useTheme
+  useTheme,
 } from "@mui/icons-material";
 import FlexBetween from "./FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "state";
 import { useState } from "react";
+import { AppBar, IconButton, Toolbar } from "@mui/material";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const theme = useTheme();
-  return <div>Navbar</div>;
+  return (
+    <AppBar sx={{ position: "static", background: "none", boxShadow: "none" }}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <FlexBetween>
+          <IconButton onClick={() => console.log("open/close sidebar")}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween></FlexBetween>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
