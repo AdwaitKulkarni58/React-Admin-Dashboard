26e43e16a48911da1376f9e5d344cf50a0c6321a
Adwait <adwait.kul.2018@gmail.com>
2023-03-27 16:51:23 -0700

    create get route

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { getUser } from "../controllers/general";
 
-
 const router = express.Router();
+
+router.get("users/:id", getUser);
 
 export default router;
