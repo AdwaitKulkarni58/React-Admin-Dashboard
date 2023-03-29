232c49eeedd7ba2b4db9bc928455f2085b4c4213
Adwait <adwait.kul.2018@gmail.com>
2023-03-28 17:48:28 -0700

    start api call work

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -5,10 +5,13 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
+import { useGetUserQuery } from "state/api";
 
 const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
       <Sidebar
