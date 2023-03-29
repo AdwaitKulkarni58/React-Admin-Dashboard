856372e1af53e57b32c0a03ce89d9384822c2ebf
Adwait <adwait.kul.2018@gmail.com>
2023-03-28 17:52:06 -0700

    comment db save method

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -11,6 +11,7 @@ const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
