748d17dfaf3caebdc56bd74a4c1815655f66b723
Adwait <adwait.kul.2018@gmail.com>
2023-03-28 18:10:26 -0700

    navbar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -24,7 +24,7 @@ const Layout = () => {
       />
       <Box>
         <Navbar
-        user={data || {}}
+          user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
